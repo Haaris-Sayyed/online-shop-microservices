45ad5f3a76504ff2d45de1d747f7a71b91d49e66
Haaris Sayyed <haaris7200@gmail.com>
2025-03-19 22:32:39 +0530

    Add code for interservice communication and wiremock support

--- order-service/src/main/java/com/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.microservices.order.service;
 
+import com.microservices.order.client.InventoryClient;
 import com.microservices.order.dto.OrderRequest;
 import com.microservices.order.model.Order;
 import com.microservices.order.repository.OrderRepository;
@@ -11,7 +12,14 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private final InventoryClient inventoryClient;
+
     public String placeOrder(OrderRequest orderRequest){
+       boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+       if(!isProductInStock){
+           throw new RuntimeException("Product with skucode " + orderRequest.skuCode() + " is not in stock");
+       }
         Order order = new Order();
         order.setOrderNumber(orderRequest.orderNumber());
         order.setSkuCode(orderRequest.skuCode());
@@ -19,5 +27,6 @@ public class OrderService {
         order.setQuantity(orderRequest.quantity());
         orderRepository.save(order);
         return "Order placed successfully.";
+
     }
 }
--- order-service/src/test/java/com/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/microservices/order/OrderServiceApplicationTests.java
@@ -1,11 +1,13 @@
 package com.microservices.order;
 
+import com.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,6 +15,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@LocalServerPort
@@ -33,6 +36,8 @@ class OrderServiceApplicationTests {
 				    "quantity": 1
 				}
 				""";
+		InventoryClientStub.stubInventoryCall("iphone_15",1);
+
 		String responseBodyString = RestAssured.given()
 				.contentType("application/json")
 				.body(submitOrderJson)
