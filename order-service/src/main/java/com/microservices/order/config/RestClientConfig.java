92f462563db6336df6ac24a3e1196948294d5638
Haaris Sayyed <haaris7200@gmail.com>
2025-04-19 19:59:15 +0530

    Add code to make microservices resilient

--- order-service/src/main/java/com/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/microservices/order/config/RestClientConfig.java
@@ -1,10 +1,11 @@
 package com.microservices.order.config;
 
 import com.microservices.order.client.InventoryClient;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -19,9 +20,17 @@ public class RestClientConfig {
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory(){
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(3000); // in milliseconds
+        factory.setReadTimeout(3000);
+        return factory;
     }
 }
