65da52abdd4d2ced6f14f026e4194551805fb5ab
Haaris Sayyed <haaris7200@gmail.com>
2025-04-30 23:43:35 +0530

    Integrate kafka with schema registry

--- order-service/src/main/java/com/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/microservices/order/config/RestClientConfig.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -17,7 +17,7 @@ public class RestClientConfig {
     private String inventoryServiceUrl;
 
     @Bean
-    public InventoryClient inventoryClient(){
+    public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
                 .requestFactory(getClientRequestFactory())
@@ -28,9 +28,10 @@ public class RestClientConfig {
     }
 
     private ClientHttpRequestFactory getClientRequestFactory(){
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(3000); // in milliseconds
         factory.setReadTimeout(3000);
         return factory;
     }
+
 }
--- order-service/src/main/java/com/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/microservices/order/service/OrderService.java
@@ -2,18 +2,26 @@ package com.microservices.order.service;
 
 import com.microservices.order.client.InventoryClient;
 import com.microservices.order.dto.OrderRequest;
+import com.microservices.order.event.OrderPlacedEvent;
 import com.microservices.order.model.Order;
 import com.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
 
     private final InventoryClient inventoryClient;
+
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest){
        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
@@ -21,11 +29,21 @@ public class OrderService {
            throw new RuntimeException("Product with skucode " + orderRequest.skuCode() + " is not in stock");
        }
         Order order = new Order();
-        order.setOrderNumber(orderRequest.orderNumber());
+        order.setOrderNumber(UUID.randomUUID().toString());
         order.setSkuCode(orderRequest.skuCode());
         order.setPrice(orderRequest.price());
         order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+
+        // Send the message to Kafka Topic
+        OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+        orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+        orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+        orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+        orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+        log.info("Start - Sending OrderPlacedEvent {} to kafka topic order-placed", orderPlacedEvent);
+        kafkaTemplate.send("order-placed",orderPlacedEvent);
+        log.info("End - Sending OrderPlacedEvent {} to kafka topic order-placed", orderPlacedEvent);
+
         return "Order placed successfully.";
 
     }
